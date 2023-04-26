5e8f2b9e342940955a4213d99327f7eb993375a0
tymwitko <tymon.kobylecki@gmail.com>
2023-04-26 18:41:53 +0200

    Enter correct colors from color palette on figma

--- mobile/lib/resources/colors.dart
+++ mobile/lib/resources/colors.dart
@@ -37,12 +37,34 @@ abstract class RAColors {
     }
   }
 
-  // TODO: add project colors
-  RAColor get primary;
+  RAColor get background_light;
+  RAColor get background_light_secondary;
+  RAColor get background_dark;
+  RAColor get background_dark_secondary;
+
+  RAColor get highlight_red;
+  RAColor get highlight_blue;
+  RAColor get highlight_yellow;
+  RAColor get highlight_green;
+  RAColor get highlight_purple;
+  RAColor get highlight_orange;
 }
 
 class _ColorsLight extends RAColors {
   const _ColorsLight() : super._();
   @override
-  RAColor get primary => const RAColor._(0xFFFFFFFF);
+  // backgrounds
+  RAColor get background_light => const RAColor._(0xFFFFF4DB);
+  RAColor get background_light_secondary => const RAColor._(0xFFEADFC8);
+  RAColor get background_dark => const RAColor._(0xFF302318);
+  RAColor get background_dark_secondary => const RAColor._(0xFF584E40);
+
+  // highlights
+  RAColor get highlight_red => const RAColor._(0xFFE15B58);
+  RAColor get highlight_blue => const RAColor._(0xFF7690B6);
+  RAColor get highlight_yellow => const RAColor._(0xFFDED93D);
+  RAColor get highlight_green => const RAColor._(0xFF6DB79B);
+  RAColor get highlight_purple => const RAColor._(0xFFD198FE);
+  RAColor get highlight_orange => const RAColor._(0xFFFFA573);
+  RAColor get highlight_magenta => const RAColor._(0xFFFD95FF);
 }
