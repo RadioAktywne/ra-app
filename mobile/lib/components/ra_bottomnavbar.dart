44871bd59f00664d89b195e082aaf119f1b0fe8b
julianuziemblo <julianuziemblo@wp.pl>
2023-07-30 18:28:20 +0200

    Fix bottom navigation bar - all icons in the bottom navbar are taken from the flutter library - fixed all widths and heights in the bottom navbar to be the same as in   Figma - fixed label colision issue

--- mobile/lib/components/ra_bottomnavbar.dart
+++ mobile/lib/components/ra_bottomnavbar.dart
@@ -1,64 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:radioaktywne/extensions/build_context.dart';
 
-import 'package:radioaktywne/resources/assets.gen.dart';
-import 'package:radioaktywne/resources/colors.dart';
-
 class RaBottomNavigationBar extends StatelessWidget {
   const RaBottomNavigationBar({
     super.key,
     required this.currentIndex,
+    this.borderWidth = 5,
   });
 
   final int currentIndex;
+  final double borderWidth;
 
-  static final labels = [
-    'home',
-    'mic',
-    'album',
-    'article',
-  ];
-
-  SizedBox _loadIconWithName(String name) {
-    return SizedBox(
-      width: 35,
-      height: 35,
-      child: SvgGenImage('assets/icons/$name.svg').svg(),
-    );
-  }
-
-  List<BottomNavigationBarItem> _makeList(List<String> labels) {
-    final list = <BottomNavigationBarItem>[];
-    for (final label in labels) {
-      list.add(
-        BottomNavigationBarItem(
-          icon: _loadIconWithName('${label}_outline'),
-          activeIcon: _loadIconWithName(label),
-          label: label,
-        ),
-      );
-    }
-    return list;
-  }
+  // ? Leaving this as a reminder...
+  // static const labels = <String>['home', 'mic', 'album', 'article'];
+  // static const widths = <double>[30, 21, 32.5, 27];
+  // static const heights = <double>[26, 28.5, 32.5, 27];
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(top: 8),
-      height: 79,
-      color: RAColors.of(context).highlightGreen,
-      child: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: context.colors.backgroundDark,
-        selectedItemColor: context.colors.highlightGreen,
-        unselectedItemColor: context.colors.highlightGreen,
-        currentIndex: currentIndex,
-        onTap: (index) {
-          // TODO: Implement navigation
-        },
-        items: _makeList(labels),
+      padding: EdgeInsets.only(top: borderWidth),
+      height: 50,
+      color: context.colors.highlightGreen,
+      child: Container(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        color: context.colors.backgroundDark,
+        child: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          elevation: 0,
+          selectedLabelStyle: const TextStyle(fontSize: 0),
+          unselectedLabelStyle: const TextStyle(fontSize: 0),
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: context.colors.backgroundDark,
+          selectedItemColor: context.colors.highlightGreen,
+          unselectedItemColor: context.colors.highlightGreen,
+          currentIndex: currentIndex,
+          onTap: (index) {
+            // TODO: Implement navigation
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined, size: 30),
+              activeIcon: Icon(Icons.home, size: 30),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.mic_none_outlined, size: 28.5),
+              activeIcon: Icon(Icons.mic, size: 28.5),
+              label: 'mic',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.album_outlined, size: 32.5),
+              activeIcon: Icon(Icons.album, size: 32.5),
+              label: 'album',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.article_outlined, size: 27),
+              activeIcon: Icon(Icons.article, size: 27),
+              label: 'article',
+            ),
+          ],
+        ),
       ),
     );
   }
