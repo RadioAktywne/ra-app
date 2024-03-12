502b014bc3ba4a98ee691b1ff8604e3f8d877533
julianuziemblo <julianuziemblo@wp.pl>
2024-03-12 13:33:05 +0100

    Remove unused import

--- mobile/lib/components/ramowka/ramowka_widget.dart
+++ mobile/lib/components/ramowka/ramowka_widget.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:radioaktywne/components/color_shadowed_card.dart';
 import 'package:radioaktywne/components/ramowka/ramowka_list.dart';
