8ba81386428a54a0e38580fdc11fec74bd43718e
julianuziemblo <julianuziemblo@wp.pl>
2024-03-12 13:32:46 +0100

    Remove unused variable

--- mobile/lib/components/ramowka/ramowka_widget.dart
+++ mobile/lib/components/ramowka/ramowka_widget.dart
@@ -12,15 +12,8 @@ import 'package:radioaktywne/extensions/extensions.dart';
 class RamowkaWidget extends StatelessWidget {
   const RamowkaWidget({
     super.key,
-    this.dataSource,
     this.timeout = const Duration(seconds: 7),
   });
-
-  /// Asynchronous source of data in the widget.
-  ///
-  /// On default, the data is pulled from the radioaktywne.pl api.
-  /// If specified, the data will be pulled from the specified source.
-  final Future<void>? dataSource;
 
   /// Timeout for the fetching function.
   final Duration timeout;
