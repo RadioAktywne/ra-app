a55ec7923346b3ad433cb01cada829c5a57a7393
tomek <2002tomo@gmail.com>
2024-06-22 15:35:43 +0200

    lazy loading is working rigtht now

--- mobile/lib/pages/article_selection_page.dart
+++ mobile/lib/pages/article_selection_page.dart
@@ -12,7 +12,7 @@ import 'package:radioaktywne/resources/fetch_data.dart';
 import 'package:radioaktywne/resources/ra_page_constraints.dart';
 import 'package:radioaktywne/router/ra_routes.dart';
 
-class ArticleSelectionPage extends StatelessWidget {
+class ArticleSelectionPage extends StatefulWidget {
   const ArticleSelectionPage({
     super.key,
     this.timeout = const Duration(seconds: 15),
@@ -20,17 +20,67 @@ class ArticleSelectionPage extends StatelessWidget {
 
   final Duration timeout;
 
-  // Single URL that returns all articles
-  // TODO: Lazy loading and loading every article not one page of articles
-  static final Uri _infoUrl = Uri.parse(
-    'https://radioaktywne.pl/wp-json/wp/v2/posts?_embed=true&page=1&per_page=16',
+  @override
+  _ArticleSelectionPageState createState() => _ArticleSelectionPageState();
+}
+
+class _ArticleSelectionPageState extends State<ArticleSelectionPage> {
+  final ScrollController _scrollController = ScrollController();
+  final List<ArticleInfo> _articles = [];
+  int _currentPage = 1;
+  bool _isLoading = false;
+  bool _hasMore = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchArticles();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController..removeListener(_onScroll)
+    ..dispose();
+    super.dispose();
+  }
+
+Future<Iterable<ArticleInfo>> _fetchArticles() async {
+  if (_isLoading || !_hasMore) {
+    return [];
+  }
+  setState(() => _isLoading = true);
+
+  final pageUri = Uri.parse(
+    'https://radioaktywne.pl/wp-json/wp/v2/posts?_embed=true&page=$_currentPage&per_page=16',
   );
 
-  Future<Iterable<ArticleInfo>> _fetchArticles() async {
-    try {
-      return await fetchData(_infoUrl, ArticleInfo.fromJson);
-    } on TimeoutException catch (_) {
-      return [];
+  try {
+    final newArticles = await fetchData(pageUri, ArticleInfo.fromJson);
+    setState(() {
+      _articles.addAll(newArticles);
+      _currentPage++;
+      _isLoading = false;
+      _hasMore = newArticles.isNotEmpty;
+    });
+    return newArticles;
+  } on TimeoutException catch (_) {
+    setState(() => _isLoading = false);
+    return [];
+  } catch (e) {
+    setState(() {
+      _isLoading = false;
+      _hasMore = false;
+    });
+    return [];
+  }
+}
+
+
+  void _onScroll() {
+    if (_scrollController.position.pixels >=
+        _scrollController.position.maxScrollExtent - 200 && !_isLoading) {
+      _fetchArticles();
     }
   }
 
@@ -46,20 +96,21 @@ class ArticleSelectionPage extends StatelessWidget {
     return RefreshableFetchWidget(
       onFetch: _fetchArticles,
       defaultData: const <ArticleInfo>[],
-      hasData: (articles) => articles.isNotEmpty,
+      hasData: (articles) => _articles.isNotEmpty,
       loadingBuilder: (context, snapshot) => const _ArticleSelectionWaiting(),
       errorBuilder: (context) => const _ArticleSelectionNoData(),
       builder: (context, articles) {
         return GridView.builder(
+          controller: _scrollController,
           padding: const EdgeInsets.only(
             bottom: 1.5 * RaPageConstraints.radioPlayerHeight,
           ),
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
           ),
-          itemCount: articles.length,
+          itemCount: _articles.length,
           itemBuilder: (context, index) {
-            final article = articles.elementAt(index);
+            final article = _articles.elementAt(index);
             return Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
               child: GestureDetector(
