e77c77f145345080e49ab46ff50cac9cf56e12a2
tomek <2002tomo@gmail.com>
2024-06-22 23:21:18 +0200

    replaced statefull widget with hooks

--- mobile/lib/pages/article_selection_page.dart
+++ mobile/lib/pages/article_selection_page.dart
@@ -3,9 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 import 'package:go_router/go_router.dart';
+import 'package:leancode_hooks/leancode_hooks.dart';
 import 'package:radioaktywne/components/utility/color_shadowed_card_2.dart';
 import 'package:radioaktywne/components/utility/ra_progress_indicator.dart';
-import 'package:radioaktywne/components/utility/refreshable_fetch_widget.dart';
 import 'package:radioaktywne/extensions/extensions.dart';
 import 'package:radioaktywne/models/article_info.dart';
 import 'package:radioaktywne/resources/fetch_data.dart';
@@ -13,7 +13,7 @@ import 'package:radioaktywne/resources/ra_page_constraints.dart';
 import 'package:radioaktywne/resources/shadow_color.dart';
 import 'package:radioaktywne/router/ra_routes.dart';
 
-class ArticleSelectionPage extends StatefulWidget {
+class ArticleSelectionPage extends HookWidget {
   const ArticleSelectionPage({
     super.key,
     this.timeout = const Duration(seconds: 15),
@@ -22,148 +22,106 @@ class ArticleSelectionPage extends StatefulWidget {
   final Duration timeout;
 
   @override
-  _ArticleSelectionPageState createState() => _ArticleSelectionPageState();
-}
-
-class _ArticleSelectionPageState extends State<ArticleSelectionPage> {
-  final ScrollController _scrollController = ScrollController();
-  final List<ArticleInfo> _articles = [];
-  int _currentPage = 1;
-  bool _isLoading = false;
-  bool _hasMore = true;
-  bool _hasError = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchArticles();
-    _scrollController.addListener(_onScroll);
-  }
-
-  @override
-  void dispose() {
-    _scrollController
-      ..removeListener(_onScroll)
-      ..dispose();
-    super.dispose();
-  }
-
-  Future<Iterable<ArticleInfo>> _fetchArticles() async {
-    if (_isLoading || !_hasMore) {
-      return [];
-    }
-    setState(() {
-      _isLoading = true;
-      _hasError = false;
-    });
-
-    final pageUri = Uri.parse(
-      'https://radioaktywne.pl/wp-json/wp/v2/posts?_embed=true&page=$_currentPage&per_page=16',
-    );
-
-    try {
-      final newArticles = await fetchData(pageUri, ArticleInfo.fromJson);
-      setState(() {
-        _articles.addAll(newArticles);
-        _currentPage++;
-        _isLoading = false;
-        _hasMore = newArticles.isNotEmpty;
-      });
-      return newArticles;
-    } on TimeoutException catch (_) {
-      setState(() {
-        _isLoading = false;
-        _hasError = true;
-      });
-      return [];
-    } catch (e) {
-      setState(() {
-        _isLoading = false;
-        _hasError = true;
-        _hasMore = false;
-      });
-      return [];
-    }
-  }
-
-
-  void _onScroll() {
-    if (_scrollController.position.pixels >=
-            _scrollController.position.maxScrollExtent - 200 &&
-        !_isLoading) {
-      _fetchArticles();
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return RefreshableFetchWidget(
-      onFetch: _fetchArticles,
-      defaultData: [ArticleInfo.empty()],
-      hasData: (articles) => _articles.isNotEmpty,
-      loadingBuilder: (context, snapshot) {
-        if (_isLoading) {
-          return const _ArticleSelectionWaiting();
+    final scrollController = useScrollController();
+    final articles = useState<Iterable<ArticleInfo>>([]);
+    final currentPage = useState(1);
+    final isLoading = useState(false);
+    final hasMore = useState(true);
+    final hasError = useState(false);
+
+    Future<void> fetchArticles() async {
+      if (isLoading.value || !hasMore.value) {
+        return;
+      }
+      isLoading.value = true;
+      hasError.value = false;
+
+      final pageUri = Uri.parse(
+        'https://radioaktywne.pl/wp-json/wp/v2/posts?_embed=true&page=${currentPage.value}&per_page=16',
+      );
+
+      try {
+        final newArticles = await fetchData(pageUri, ArticleInfo.fromJson);
+        if (newArticles.isNotEmpty) {
+          currentPage.value++;
+          articles.value = [...articles.value, ...newArticles];
         } else {
-          return Container();
+          hasMore.value = false;
         }
-      },
-      errorBuilder: (context) {
-        if (_hasError) {
-          return const _ArticleSelectionNoData();
-        } else {
-          return Container();
+      } on TimeoutException catch (_) {
+        hasError.value = true;
+      } catch (e) {
+        hasError.value = true;
+        hasMore.value = false;
+      } finally {
+        isLoading.value = false;
+      }
+    }
+
+    useEffect(() {
+      fetchArticles();
+      scrollController.addListener(() {
+        if (scrollController.position.pixels >=
+                scrollController.position.maxScrollExtent - 200 &&
+            !isLoading.value && hasMore.value) {
+          fetchArticles();
         }
-      },
-      builder: (context, articles) {
-        return Padding(
-          padding: const EdgeInsets.only(bottom: 50), // Helps with the player not covering the last article
-          child: GridView.builder(
-            controller: _scrollController,
-            padding: const EdgeInsets.all(20), // Space around the grid
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 20, // Space between columns
-              mainAxisSpacing: 20, // Space between rows
+      });
+      return () => scrollController.dispose;
+    }, [],);
+
+    if (isLoading.value && articles.value.isEmpty) {
+      return const _ArticleSelectionWaiting();
+    } else if (hasError.value && articles.value.isEmpty) {
+      return const _ArticleSelectionNoData();
+    }
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 50), // Helps with the player not covering the last article
+      child: GridView.builder(
+        controller: scrollController,
+        padding: const EdgeInsets.all(20), // Space around the grid
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 20, // Space between columns
+          mainAxisSpacing: 20, // Space between rows
+        ),
+        itemCount: articles.value.length,
+        itemBuilder: (context, index) {
+          final article = articles.value.elementAt(index);
+          return GestureDetector(
+            onTap: () => context.push(
+              RaRoutes.articleId(article.id),
+              extra: article,
             ),
-            itemCount: _articles.length,
-            itemBuilder: (context, index) {
-              final article = _articles.elementAt(index);
-              return GestureDetector(
-                onTap: () => context.push(
-                  RaRoutes.articleId(article.id),
-                  extra: article,
+            child: ColorShadowedCard2(
+              shadowColor: shadowColor(context, index),
+              footer: DefaultTextStyle(
+                style: context.textStyles.textSmall.copyWith(
+                  color: context.colors.highlightGreen,
                 ),
-                child: ColorShadowedCard2(
-                  shadowColor: shadowColor(context, index),
-                  footer: DefaultTextStyle(
-                    style: context.textStyles.textSmall.copyWith(
-                      color: context.colors.highlightGreen,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(5),
-                      child: HtmlWidget(article.title),
-                    ),
-                  ),
-                  child: Image.network(
-                    article.thumbnail,
-                    fit: BoxFit.fill,
-                    errorBuilder: (context, error, stackTrace) => AspectRatio(
-                      aspectRatio: 1,
-                      child: Center(
-                        child: Image.asset(
-                          'assets/defaultMedia.png',
-                          fit: BoxFit.fill,
-                          ),
-                      ),
+                child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: HtmlWidget(article.title),
+                ),
+              ),
+              child: Image.network(
+                article.thumbnail,
+                fit: BoxFit.fill,
+                errorBuilder: (context, error, stackTrace) => AspectRatio(
+                  aspectRatio: 1,
+                  child: Center(
+                    child: Image.asset(
+                      'assets/defaultMedia.png',
+                      fit: BoxFit.fill,
                     ),
                   ),
                 ),
-              );
-            },
-          ),
-        );
-      },
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
