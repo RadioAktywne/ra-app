f71b6e343f5f571bd9a0f9dc74a4fe63a5d16e3d
tomek <2002tomo@gmail.com>
2024-10-22 12:41:59 +0200

    added working play button, needs some improvemnt

--- mobile/lib/pages/single_recording_page.dart
+++ mobile/lib/pages/single_recording_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:radioaktywne/components/ra_playbutton.dart';
+import 'package:radioaktywne/components/ra_player/ra_player_handler.dart';
+import 'package:radioaktywne/components/ra_player/ra_player_recources.dart';
 import 'package:radioaktywne/components/utility/custom_padding_html_widget.dart';
 import 'package:radioaktywne/components/utility/ra_progress_indicator.dart';
 import 'package:radioaktywne/extensions/extensions.dart';
@@ -6,6 +10,7 @@ import 'package:radioaktywne/models/recording_info.dart';
 import 'package:radioaktywne/resources/fetch_data.dart';
 import 'package:radioaktywne/resources/ra_links.dart';
 import 'package:radioaktywne/resources/ra_page_constraints.dart';
+import 'package:radioaktywne/state/audio_handler_cubit.dart';
 
 /// Page displaying a single recording.
 class SingleRecordingPage extends StatefulWidget {
@@ -53,53 +58,73 @@ class _SingleRecordingPageState extends State<SingleRecordingPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: context.colors.backgroundLight,
-      child: Padding(
-        padding: RaPageConstraints.outerTextPagePadding,
-        child: ListView(
-          children: [
-            _spaceFromTop,
-            Image.network(
-              widget.recording.fullImagePath,
-              loadingBuilder: (context, child, loadingProgress) =>
-                  loadingProgress == null
-                      ? child
-                      : Container(
-                          color: context.colors.backgroundDarkSecondary,
-                          child: const RaProgressIndicator(),
-                        ),
-              errorBuilder: (_, __, ___) => Center(
-                child: Image.asset('assets/defaultMedia.png'),
-              ),
-            ),
-            _emptySpace,
-            Container(
-              color: context.colors.backgroundDark,
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: CustomPaddingHtmlWidget(
-                    style: context.textStyles.textMedium.copyWith(
-                      color: context.colors.backgroundLight,
+    return BlocBuilder<AudioHandlerCubit, RaPlayerHandler>(
+      builder: (context, audioHandler) {
+        return Container(
+          color: context.colors.backgroundLight,
+          child: Padding(
+            padding: RaPageConstraints.outerTextPagePadding,
+            child: ListView(
+              children: [
+                _spaceFromTop,
+                // Use Stack to overlay the RaPlayButton on the image
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Image.network(
+                      widget.recording.fullImagePath,
+                      loadingBuilder: (context, child, loadingProgress) =>
+                          loadingProgress == null
+                              ? child
+                              : Container(
+                                  color: context.colors.backgroundDarkSecondary,
+                                  child: const RaProgressIndicator(),
+                                ),
+                      errorBuilder: (_, __, ___) => Center(
+                        child: Image.asset('assets/defaultMedia.png'),
+                      ),
                     ),
-                    htmlContent: widget.recording.title,
+                    // RaPlayButton in the center of the image
+                    RaPlayButton(
+                      size: 60, // Adjust the size as needed
+                      onPressed: () async => audioHandler.playMediaItem(
+                        widget.recording.mediaItem,
+                        mediaKind: MediaKind.recording,
+                      ),
+                      playing: audioHandler.playbackState.value.playing,
+                      audioProcessingState: audioHandler.playbackState.value.processingState,
+                    ),
+                  ],
+                ),
+                _emptySpace,
+                Container(
+                  color: context.colors.backgroundDark,
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: CustomPaddingHtmlWidget(
+                        style: context.textStyles.textMedium.copyWith(
+                          color: context.colors.backgroundLight,
+                        ),
+                        htmlContent: widget.recording.title,
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                _emptySpace,
+                CustomPaddingHtmlWidget(
+                  style: context.textStyles.textSmallGreen.copyWith(
+                    color: context.colors.backgroundDark,
+                  ),
+                  htmlContent: widget.recording.description,
+                ),
+                SizedBox(height: context.playerPaddingValue),
+              ],
             ),
-            _emptySpace,
-            CustomPaddingHtmlWidget(
-              style: context.textStyles.textSmallGreen.copyWith(
-                color: context.colors.backgroundDark,
-              ),
-              htmlContent: widget.recording.description,
-            ),
-            SizedBox(height: context.playerPaddingValue),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
